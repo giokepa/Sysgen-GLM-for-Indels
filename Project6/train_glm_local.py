f2c9ef78a71f700ec659cba1479df8c849eb4db1
AmelieLaura <94526113+AmelieLaura@users.noreply.github.com>
2025-12-21 20:47:02 +0100

    Update train_glm_local.py

--- Project6/train_glm_local.py
+++ Project6/train_glm_local.py
@@ -2,33 +2,6 @@ import os
 import sys
 
 PROJECT_ROOT = "/Users/amelielaura/Documents/Project6"
-sys.path.insert(0, PROJECT_ROOT)
-
-from lib.glm_model_new import GLMModel
-
-
-def main():
-    fasta_path = os.path.join(
-        PROJECT_ROOT,
-        "data",
-        "augumented_sequence_size100000_length150_deletions0.1_nodeletionseq0.25.fasta"
-    )
-    model_out = os.path.join(PROJECT_ROOT, "model_out")
-
-    glm = GLMModel(model_path=model_out, fasta_file=fasta_path, max_seq_length=256)
-
-    # small first test (increase later)
-    glm.train(epochs=1, batch_size=16, lr=2e-4)
-
-
-if __name__ == "__main__":
-    main()
-
-
-import os
-import sys
-
-PROJECT_ROOT = "/Users/amelielaura/Documents/Project6"
 sys.path.insert(0, os.path.join(PROJECT_ROOT, "lib"))
 
 from glm_model_new import GLMModel
